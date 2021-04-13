d672f654c589b8216b974766a1ff9edfda966bf1
laurennrmoore <lmooreintech2021@gmail.com>
2021-04-13 17:19:28 -0400

    hwpush

---
+++ Functionhw.js
@@ -0,0 +1,48 @@
+
+// create (4) problems
+// Example:
+
+function addTwo(x,y){
+    answer = x+y;
+    console.log (answer);
+}
+
+//this call returns the sum of the two numbers provided inside of the parenthesis
+addTwo(2,7);
+addTwo(7,7);
+
+
+var a = 5;
+var b = 10;
+
+function addThree(a,b){
+    answer = a+b;
+    console.log (answer);
+}
+
+function subtractThree(c,d){
+  answer = c-b;
+  console.log (answer);  
+}
+subtractThree(7,6)
+subtractThree(5,10)
+function multiplyFour(e,f){
+  answer = e*f;
+  console.log (answer);  
+}
+multiplyFour(2,4)
+multiplyFour(25,50)
+function divideTwo(){
+  answer = g/h;
+  console.log (answer);   
+}
+
+divideTwo(50,25)
+divideTwo(2,10)
+function divideTwo(){
+answer = i,j;
+console.log (answer);
+}
+
+
+
